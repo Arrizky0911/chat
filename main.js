a4d36b9d8da1b2258a12dc346fbc0be5d87e75f9
arrizkyaditha <arrizkyaditha@gmail.com>
2024-08-28 21:06:03 +0000

    finish

--- main.js
+++ main.js
@@ -6,9 +6,6 @@ import {ChatPromptTemplate, MessagesPlaceholder} from '@langchain/core/prompts';
 import { RunnableSequence, RunnableWithMessageHistory } from '@langchain/core/runnables';
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { ChatMessageHistory } from 'langchain/memory';
-import pg from 'pg';
-import { PostgresChatMessageHistory } from "@langchain/community/stores/message/postgres";
-// import {Connector} from '@google-cloud/cloud-sql-connector';
 
 const form = document.querySelector('form');
 const promptInput = document.querySelector('input[name="prompt"]');
@@ -21,18 +18,6 @@ form.onsubmit = async ev => {
   output.textContent = 'Generating...';
 
   try {
-    const poolConfig = {
-      host: "localhost",
-      port: 5432,
-      user: "postgres",
-      password: "Anthony21",
-      database: "aic",
-    };
-
-    console.log("poolConfig ok");
-    
-    const pool = new pg.Pool(poolConfig);
-
     const prompt = ChatPromptTemplate.fromMessages([
       [
         "system",
@@ -44,7 +29,7 @@ form.onsubmit = async ev => {
 
     // Call the multimodal model, and get a stream of results
     const model = new ChatGoogleGenerativeAI({
-      apiKey: 'AIzaSyCj8_4rGPoeHo_GbUQmH3qhI2s_8wGU2OU',
+      apiKey: 'AIzaSyDWDRCfoBi5WRy_8TYroLKuZvVQiG8WrvA',
       modelName: 'gemini-1.5-flash', // or gemini-1.5-pro
       safetySettings: [
         {
@@ -62,31 +47,21 @@ form.onsubmit = async ev => {
 
     const chat = new RunnableWithMessageHistory({
       runnable: chain,
-      // getMessageHistory: (_sessionId) => history,
-      getMessageHistory: async (sessionId) => {
-        const chatHistory = new PostgresChatMessageHistory({
-          sessionId,
-          pool,
-          // Can also pass `poolConfig` to initialize the pool internally,
-          // but easier to call `.end()` at the end later.
-        });
-        return chatHistory;
-      },
+      getMessageHistory: (_sessionId) => history,
       inputMessagesKey: 'input',
       historyMessagesKey: "chat_history",
     })
 
-    const response = await chat.invoke(
+    await chat.invoke(
       {
         input: promptInput.value,
       },
       {configurable: {sessionId: "unsued",},},
     );
 
-    console.log("something wrong");
-
-    output.textContent = response;
-    await pool.end();
+    const message = await history.getMessages();
+    console.log(JSON.stringify(message));
+    output.textContent = message.map(m => `${m.lc_id[2] === "HumanMessage" ? "You" : "AI"} ${m.content} test` ).join('\n');
   } catch (e) {
     output.innerHTML += '<hr>' + e;
   }
